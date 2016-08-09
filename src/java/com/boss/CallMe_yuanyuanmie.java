f0d4e61724780ec40f7f096c925cc5997ef3f428
Gary_Liu <512180587@qq.com>
2016-08-09 15:18:56 +0800

    2016-15：18分，尝试使用GIT

---
+++ src/java/com/boss/CallMe_yuanyuanmie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.boss;
+
+/**
+ *
+ * @author GaryLiu
+ */
+public class CallMe_yuanyuanmie {
+    //让我们荡起双桨，友谊的小船说翻就翻
+}
